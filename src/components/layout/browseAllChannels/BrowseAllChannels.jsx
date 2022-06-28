9cad53610a98cee467e66cdf57986789c8c5cfbd
hudir <hudirybw@gmail.com>
2022-06-28 15:22:03 +0200

    info

--- src/components/layout/browseAllChannels/BrowseAllChannels.jsx
+++ src/components/layout/browseAllChannels/BrowseAllChannels.jsx
@@ -1,28 +1,48 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Context } from '../../../store/Context'
 
 export default function BrowseAllChannels() {
-    const {state} = useContext(Context);
+    const {state, dispatch} = useContext(Context);
 
     const [channelToRender, setChannelToRender] = useState(null)
 
     useEffect(()=>{
         const publicChannel = state.channels.filter(el=>el.private===false || el.members.some(id=>id===state.currentUser.userID))
         setChannelToRender(publicChannel)
-    }, [])
+    }, [state.channels, state.currentUserChannels])
+
+    const leaveChannel=(channelName, notPublic)=>dispatch({
+        type: "LEAVE_CHANNEL",
+        name: channelName,
+        private: notPublic
+    })
+
+    const navi = useNavigate()
+
+    const joinChannel=(channelName)=>{
+        dispatch({
+        type: "JOIN_CHANNEL",
+        name: channelName
+        })
+        navi('../'+channelName)
+    }
 
   return (
     <div>
-        {channelToRender && channelToRender.map((el,i)=>(<Link key={i} to={`../${el.channelName}`}>
-            <h3>{el.channelName}</h3>
+        {channelToRender && channelToRender.map((el,i)=>(<div key={i}>
+
+            <Link to={`../${el.channelName}`}><h3>{el.channelName}</h3></Link>
+            
 
             {el.members.some(x=>x===state.currentUser.userID) ? <span>Joined</span> : null}
 
             <span>{el.members.length>1 ? <span>{el.members.length} members</span> : <span>{el.members.length} member</span>}</span>
 
-            <span>{el.description}</span>   
-        </Link>))
+            <span>{el.description}</span> 
+
+            {el.members.some(x=>x===state.currentUser.userID) ? <button onClick={e=>leaveChannel(el.channelName, el.private)}>Leave Channel</button> : <button onClick={e=>joinChannel(el.channelName, el.private)}>Join Channel</button> }  
+        </div>))
         }
     </div>
   )
