51e951a3882d5f28829e679b3bf7e21427c5b331
hudir <hudirybw@gmail.com>
2022-06-30 00:03:20 +0200

    fix logout navi to /

--- src/components/layout/sidebarAndSearchbar/Header.jsx
+++ src/components/layout/sidebarAndSearchbar/Header.jsx
@@ -1,15 +1,18 @@
 import React, { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Context } from '../../../store/Context'
 
 export default function Header() {
 
   const {state ,dispatch} = useContext(Context)
   const [logOut, setLogOut] = useState(false)
+  const navi = useNavigate()
 
   const logOutHandle =() =>{
     dispatch({
       type:'LOGOUT'
     })
+    navi('/')
 
   }
   return (
